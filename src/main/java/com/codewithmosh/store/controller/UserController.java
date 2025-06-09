e330deb07350e870c4cf1dd86aafb6f1b6f366b6
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-06-09 14:45:48 +0530

    add user retrieval endpoint to UserController

--- src/main/java/com/codewithmosh/store/controller/UserController.java
+++ src/main/java/com/codewithmosh/store/controller/UserController.java
@@ -3,7 +3,10 @@ package com.codewithmosh.store.controller;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +14,26 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getAllUser(){
         return userRepository.findAll();
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable long id){
+        var user = userRepository.findById(id).orElse(null);
+
+        if (user == null){
+//          return  ResponseEntity<User>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
+        }
+
+//        return new ResponseEntity<>(user,HttpStatus.OK);
+        return ResponseEntity.ok(user);
     }
 }
