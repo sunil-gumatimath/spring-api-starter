86b0499e3cda2349ab547bfa28c65963db5a1a71
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-06-09 13:42:31 +0530

    hello world using thymeleaf

--- src/main/java/com/codewithmosh/store/controller/HomeController.java
+++ src/main/java/com/codewithmosh/store/controller/HomeController.java
@@ -1,8 +1,20 @@
 package com.codewithmosh.store.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("fname","Ted");
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return "index.html";
+    }
 }
