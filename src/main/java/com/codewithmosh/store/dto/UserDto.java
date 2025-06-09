0e23f46b6791b7101bfb33d2c45c422961683112
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-06-09 17:35:29 +0530

    add MapStruct dependencies and update UserController to return UserDto

---
+++ src/main/java/com/codewithmosh/store/dto/UserDto.java
@@ -0,0 +1,26 @@
+package com.codewithmosh.store.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class UserDto {
+//    @JsonIgnore  to ignore field
+    @JsonProperty("user_id")
+    private Long id;
+    private  String name;
+    private String email;
+
+    private LocalDateTime createdAt;
+
+//    @JsonInclude(JsonInclude.Include.NON_NULL)
+//    private String phoneNumber;
+
+}
