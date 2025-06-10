a4b18205fb308cb0fe441284e1137f148db2a0a8
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-06-10 22:56:09 +0530

    add product retrieval methods to ProductRepository and update sorting in UserController

---
+++ src/main/java/com/codewithmosh/store/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.codewithmosh.store.controller;
+
+import com.codewithmosh.store.dto.ProductDto;
+import com.codewithmosh.store.entities.Product;
+import com.codewithmosh.store.mappers.ProductMapper;
+import com.codewithmosh.store.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @GetMapping
+    public List<ProductDto> getAllProducts(
+             @RequestParam(required = false,name = "categoryId") Byte categoryId
+    ){
+        List<Product> products;
+        if (categoryId != null){
+            products = productRepository.findByCategoryId(categoryId);
+        }else {
+            products = productRepository.findAllWithCategory();
+        }
+        return products.stream().map(productMapper::toDto).toList();
+    }
+}
